116d6297bec972c6d4313875e812054e7079a644
CoryBruce <74074337+CoryBruce@users.noreply.github.com>
2021-03-08 16:31:45 -0600

    Add files via upload
    started a main menu with a weather button
    wrote a time check func to allow a web scrap every 30 min
    writes weather data to text file to minimize hitting the website as much
    few bug fixes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import time
+from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -52,8 +53,12 @@ def organize_data(zipcode):
 
 def weather(zipcode):
     weather = organize_data(zipcode)
-    for item in weather:
-        print(item)
+    with open('weather.text','w') as f:
+        for item in weather:
+            f.write(item)
+            f.write('\n')
+            #print(item)
+    return weather
 
 def grab_stock_data():
     driver = webdriver.Firefox(executable_path=r"C:\Users\coryb\PycharmProjects\geckodriver.exe")
@@ -88,6 +93,7 @@ def new_user():
             print('Saving data...')
             list = [user, pass1, zip]
             write_txtfile(list)
+            root.destroy()
         else:
             print('Passwords dont match!')
 
@@ -125,19 +131,32 @@ def new_user():
     submit.grid(column=1, row=10)
     root.mainloop()
 
+def load_data():
+    with open('data.txt', 'r') as f:
+        data = f.read()
+        new_data = data.split('\n')
+        # print(new_data[0])
+        return new_data
 
 def login_gui():
-    def load_data():
-        with open('data.txt', 'r') as f:
-            data = f.read()
-            new_data = data.split('\n')
-            # print(new_data[0])
-            return new_data
-
     def print_info():
         userr = username_entry.get()
         pwdd = password_entry.get()
         print(userr, pwdd)
+
+    def check_login():
+        data = load_data()
+        user1 = username_entry.get()
+        pass1 = password_entry.get()
+        if user1 == data[0]:
+            if pass1 == data[1]:
+                print('Login successful')
+                root.destroy()
+                main_menu()
+            else:
+                print('Wrong password')
+        else:
+            print('No username in system')
 
     user = StringVar
     pwd = StringVar
@@ -156,17 +175,116 @@ def login_gui():
     password_label.place(x=80, y=380)
     password_entry = Entry(root, textvar=pwd)
     password_entry.place(x=240, y=380)
-    submit = Button(root, text='Submit', relief=GROOVE, font=('arial', 12, 'bold'), command=print_info)
+    submit = Button(root, text='Submit', relief=GROOVE, font=('arial', 12, 'bold'), command=check_login)
     submit.place(x=240, y=420)
+
+    root.mainloop()
+
+def main_menu():
+    def display_weather():
+        def clean_up_now():
+            now = datetime.now()
+            list = str(now).split(' ')
+            raw_time = list[1]
+            time_list = raw_time.split(':')
+            time = time_list[0]
+            return time
+        def check_time(): #need to make a check day method to check for difference in days first then if same day
+            with open('weather.text', 'r') as f:
+                data = f.read()
+                data_list = data.split('\n')
+                try:
+                    timestamp_raw = data_list[1]
+                    #timestamp = timestamp_raw.strip("Last update")
+                    #timestamp = timestamp.strip("pm CST")
+                    timestamp = timestamp_raw.split(' ')
+                    day = timestamp[2]
+                    month = timestamp[3]
+                    #print(timestamp)
+                    if timestamp[5] == 'pm':
+                        print('pm')
+                        raw_time = timestamp[4]
+                        if len(raw_time) == 4:# need to check the min and round hours up or down accordingly
+                            time = (int(raw_time[0])*100) + 1300
+                        if len(raw_time) == 5:
+                            h1 = int(raw_time[0])*1000
+                            h2 = (int(raw_time[1])*100) + 200
+                            time = h1 + h2
+                    if timestamp[5] == 'am':
+                        print('am')
+                        raw_time = timestamp[4]
+                        if len(raw_time) == 4:
+                            time = raw_time[0] * 100
+                        if len(raw_time) == 5:
+                            h1 = raw_time[0]*1000
+                            h2 = raw_time[1]*100
+                            time = h1 + h2
+                except:
+                    time = 0000
+
+                print(time)
+                now = clean_up_now()
+                now = int(now) * 100
+                print(now)
+                dif = now - time
+                print(dif)
+                if dif > 30:
+                    return True
+
+        need_weather_update = check_time()
+        current_weather = ''
+        if need_weather_update:
+            print('Updating weather')
+            weather_data = weather(zip)
+            i = 0
+            while i < 5:
+                line = weather_data[i] + '\n'
+                current_weather += line
+                i += 1
+            need_weather_update = False
+        else:
+            weather_data = []
+            with open('weather.text', 'r') as f:
+                raw_data = f.read()
+                weather_data = raw_data.split('\n')
+                i = 0
+                while i < 5:
+                    line = weather_data[i] + '\n'
+                    current_weather += line
+                    i += 1
+
+        print(current_weather)
+        text = Text(canvas, height=5, width=30)
+        text.insert(END, current_weather)
+        text.grid(column=1, row=4)
+
+    data = load_data()
+    user = data[0]
+    zip = data[2]
+    root = Tk()
+    root.title('Personal Butler')
+    canvas = Canvas(root, width=100, height=500)
+    canvas.grid(columnspan=5, rowspan=5)
+    title = Label(canvas, text=f"Welcome {user}", width=15, font=('bold', 25))
+    title.grid(column=1)
+    header = Label(canvas, text="App stuff goes here", font=('bold', 14))
+    header.grid(column=1, row=1)
+    weather_btn = Button(canvas, text="Weather", relief=GROOVE, font=('arial', 12, 'bold'),command=display_weather)
+    weather_btn.grid(column=1, row=5)
 
     root.mainloop()
 
 
 
 
-new_user()
+
+with open('data.txt', 'r') as f:
+    data = f.read()
+    print(len(data))
+    if len(data)<= 0:
+        new_user()
+        login_gui()
+    else:
+        login_gui()
+
 #stocks()
-#login_gui()
-zipcode = input("Please enter your zip: ")
-if len(zipcode) == 5:
-    weather(zipcode)
